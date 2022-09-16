c134bd990a9b8dd058551b78a092eae591d745d9
deepanshu <ldeepanshu4@gmail.com>
2022-09-16 03:31:09 -0700

    deepanshu added

---
+++ amazon-clone/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      chliya shru krta h
+    </div>
+  );
+}
+
+export default App;
---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//     apiKey: "AIzaSyAcRCub9kzRj15njN-e8u_pFKE5wz5mCsc",
+//     authDomain: "clone-b999f.firebaseapp.com",
+//     projectId: "clone-b999f",
+//     storageBucket: "clone-b999f.appspot.com",
+//     messagingSenderId: "661096601405",
+//     appId: "1:661096601405:web:d2c67133db436551c71754",
+//     measurementId: "G-P072GG5VH3"
+//   };
